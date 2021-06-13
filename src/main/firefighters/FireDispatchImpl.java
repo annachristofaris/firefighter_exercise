7fb80d6d58f5a71fd1fec08774455932ee98d23a
Anna Christofaris <annachristofaris@gmail.com>
2021-06-12 20:37:29 -0400

    WIP - initial logic implementation

--- src/main/firefighters/FireDispatchImpl.java
+++ src/main/firefighters/FireDispatchImpl.java
@@ -1,35 +1,74 @@
 package main.firefighters;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import main.api.City;
 import main.api.CityNode;
 import main.api.FireDispatch;
 import main.api.Firefighter;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import main.api.exceptions.NoFireFoundException;
+import main.api.exceptions.OutOfCityBoundsException;
 
 public class FireDispatchImpl implements FireDispatch {
+	private List<Firefighter> firefighters;
+	private City city;
+	private static Logger logger;
 
-  public FireDispatchImpl(City city) {
-    // TODO
-    throw new NotImplementedException();
-  }
+	public FireDispatchImpl(City city) {
+		this.firefighters = new ArrayList<>();
+		this.city = city;
+	}
 
-  @Override
-  public void setFirefighters(int numFirefighters) {
-    // TODO
-    throw new NotImplementedException();
-  }
+	@Override
+	public void setFirefighters(int numFirefighters) {
+		for (int i = 0; i < numFirefighters; i++) {
+			Firefighter fireFighter = new FirefighterImpl(city.getFireStation());
+			firefighters.add(fireFighter);
+		}
+	}
 
-  @Override
-  public List<Firefighter> getFirefighters() {
-    // TODO
-    throw new NotImplementedException();
-  }
+	@Override
+	public List<Firefighter> getFirefighters() {
+		return this.firefighters;
+	}
 
-  @Override
-  public void dispatchFirefighers(CityNode... burningBuildings) {
-    // TODO
-    throw new NotImplementedException();
-  }
+	@Override
+	public void dispatchFirefighers(CityNode... burningBuildings) {
+		if (this.firefighters.isEmpty()) {
+			logger.warning("No firefighters available!");
+			return;
+		}
+		// default to first firefighter in list
+		Firefighter closestFirefighter = this.firefighters.get(0);
+
+		for (CityNode burningBuilding : burningBuildings) {
+			int shortestDistance = getDistance(burningBuilding, closestFirefighter.getLocation());
+
+			for (Firefighter firefighter : this.firefighters) {
+
+				int distance = getDistance(burningBuilding, firefighter.getLocation());
+				shortestDistance = Math.min(shortestDistance, distance);
+				closestFirefighter = distance <= shortestDistance ? firefighter : closestFirefighter;
+			}
+
+			closestFirefighter.setLocation(burningBuilding);
+			closestFirefighter.addDistanceTraveled(shortestDistance);
+
+			try {
+				city.getBuilding(burningBuilding).extinguishFire();
+			} catch (OutOfCityBoundsException e) {
+				e.printStackTrace();
+			} catch (NoFireFoundException e) {
+				e.printStackTrace();
+				logger.warning("No fire is found at this location.");
+			}
+		}
+	}
+
+	private int getDistance(CityNode destination, CityNode currentLocation) {
+		return Math.abs(destination.getX() - currentLocation.getX())
+				+ Math.abs(destination.getY() - currentLocation.getY());
+	}
 }
--- src/main/firefighters/FirefighterImpl.java
+++ src/main/firefighters/FirefighterImpl.java
@@ -1,20 +1,35 @@
 package main.firefighters;
 
+import main.api.Building;
 import main.api.CityNode;
 import main.api.Firefighter;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class FirefighterImpl implements Firefighter {
+	private CityNode location;
+	private int distanceTraveled;
 
-  @Override
-  public CityNode getLocation() {
-    // TODO
-    throw new NotImplementedException();
-  }
+	public FirefighterImpl(Building fireStation) {
+		this.location = fireStation.getLocation();
+		this.distanceTraveled = 0;
+	}
 
-  @Override
-  public int distanceTraveled() {
-    // TODO
-    throw new NotImplementedException();
-  }
+	@Override
+	public CityNode getLocation() {
+		return location;
+	}
+
+	@Override
+	public int distanceTraveled() {
+		return distanceTraveled;
+	}
+
+	@Override
+	public void setLocation(CityNode destination) {
+		this.location = destination;
+	}
+	
+	public void addDistanceTraveled(int distance) {
+		this.distanceTraveled += distance;
+	}
+	
 }
